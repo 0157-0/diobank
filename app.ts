99091b7204de3769cc9162c0fa9f45a0b9e42644
Eduardo Bertozi <eduardobertozi@pop-os.localdomain>
2023-02-22 10:28:19 -0300

    descomentando app.js

--- app.ts
+++ app.ts
@@ -2,16 +2,16 @@ import { PeopleAccount } from './class/PeopleAccount'
 import { CompanyAccount } from './class/CompanyAccount'
 import { PremiumAccount } from './class/PremiumAccount'
 
-// const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Edu', 10)
-// peopleAccount.showAccountDetails()
-// peopleAccount.deposit(100)
-// peopleAccount.withDrawn(3)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Edu', 10)
+peopleAccount.showAccountDetails()
+peopleAccount.deposit(100)
+peopleAccount.withDrawn(3)
 
-// const companyAccount: CompanyAccount = new CompanyAccount('Dio', 20, 1235)
-// companyAccount.showAccountDetails()
-// companyAccount.deposit(50)
-// companyAccount.getLoan(500)
-// companyAccount.withDrawn(30)
+const companyAccount: CompanyAccount = new CompanyAccount('Dio', 20, 1235)
+companyAccount.showAccountDetails()
+companyAccount.deposit(50)
+companyAccount.getLoan(500)
+companyAccount.withDrawn(30)
 
 const premiumAccount = new PremiumAccount('Pamela', 300, 14)
 premiumAccount.showAccountDetails()
